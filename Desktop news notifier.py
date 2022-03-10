06ebe312a355e2be6d8c3a5e5b790fc13207dcc5
Rajiv Ranjan Sah <57870252+f9rajiv@users.noreply.github.com>
2022-03-10 12:32:46 +0545

    update

--- Desktop news notifier.py
+++ Desktop news notifier.py
@@ -6,17 +6,15 @@ import time
 url = 'https://english.onlinekhabar.com/feed'
 data = requests.get(url)
 soup = BeautifulSoup(data.text, 'lxml')
-# find data items
+
 news_list = soup.find_all('item')
-# list first  10  news data+654
-news_list = news_list[0:10]
-# iterate through news list
+
+
 for news in news_list:
-    # notifier to display News
+
     notification.notify(
                         title='News Update',
                         message=news.title.text,
                         timeout=1)
-    # display new update after
-    # in seconds waiting time
+  
     time.sleep(5)
